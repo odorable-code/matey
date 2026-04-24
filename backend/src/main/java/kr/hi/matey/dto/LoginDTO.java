2a1282d4432b238902fe951e42a84b55dd26643c
odorable-code <voidkknd17@gmail.com>
2026-04-24 16:51:29 +0900

    Admin API 제거

---
+++ backend/src/main/java/kr/hi/matey/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package kr.hi.matey.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record LoginDTO (
+    @Schema(description = "사용자의 아이디")
+    String userId,
+    @Schema(description = "사용자의 암호")
+    String userPw,
+
+    //AdminLogin-서연
+    @Schema(description = "로그인 상태 유지 여부")
+    boolean keepLogin
+) {}
--- backend/src/main/java/kr/hi/matey/service/MemberDetailService.java
+++ backend/src/main/java/kr/hi/matey/service/MemberDetailService.java
@@ -1,15 +1,12 @@
 package kr.hi.matey.service;
 
-import kr.hi.matey.dao.AdminsDAO;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import kr.hi.matey.dao.UserDAO;
-import kr.hi.matey.dto.MemberDTO;
 import kr.hi.matey.util.CustomUser;
-import kr.hi.matey.vo.AdminsVO;
 import kr.hi.matey.vo.UserVO;
 import lombok.AllArgsConstructor;
 
