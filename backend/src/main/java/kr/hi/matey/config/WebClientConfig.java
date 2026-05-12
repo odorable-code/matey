f619521708665545ef69b7e1a12ed27644e4332b
odorable-code <voidkknd17@gmail.com>
2026-05-12 14:50:39 +0900

    192.168.0.1:8000 연동

--- backend/src/main/java/kr/hi/matey/config/WebClientConfig.java
+++ backend/src/main/java/kr/hi/matey/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package kr.hi.matey.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${fastapi.base-url}")
+    private String fastapiBaseUrl;
+
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        return WebClient.builder()
+                .baseUrl(fastapiBaseUrl)
+                .build();
     }
 }
